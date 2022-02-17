f01829fe56d9a0e79aecaed0fe644f03c5c91da7
dialguiba <dialguiba@gmail.com>
2022-02-16 21:21:33 -0500

    🚀 add readme info and firebase conf

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,9 +1,9 @@
 import Axios from "axios";
 
 export const apiInstance = Axios.create({
-  baseURL: "/prod",
+  baseURL: process.env.REACT_APP_API_URL,
   timeout: 5000,
   headers: {
-    "x-api-key": "e9p4msN8O08nORtkLoL9C7GmRJGMjphQ6wXXFCiI",
+    "x-api-key": process.env.REACT_APP_API_KEY as string,
   },
 });
